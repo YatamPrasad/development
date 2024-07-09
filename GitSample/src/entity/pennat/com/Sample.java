12d6155db873d0dfeabd990c322ac32fbc6452e2
TARUN459 <tarunkrishna600@gmail.com>
2024-07-09 15:24:53 +0530

    My First Commit

---
+++ GitSample/src/entity/pennat/com/Sample.java
@@ -0,0 +1,14 @@
+package entity.pennat.com;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		String str = "hungry";
+		if(str.equalsIgnoreCase("hungry")) {
+			
+		}
+
+	}
+
+}
